c56e5a33dcc6241a4ab0ed35881e1b9b8da5eccc
Abdelrahman-Labib <abdolabib602@yahoo.com>
2019-03-16 20:59:22 +0200

    update

--- src/screens/app/Search/CalendarSearch.js
+++ src/screens/app/Search/CalendarSearch.js
@@ -7,8 +7,7 @@ import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 import Server from "../../../constants/config";
 import axios from "axios";
 import moment from 'moment'
-// import firebase from 'react-native-firebase';
-// import {  RemoteMessage, Notification } from 'react-native-firebase';
+import firebase, {Notification, NotificationOpen} from 'react-native-firebase';
 
 
 
@@ -21,6 +20,7 @@ export default class CalendarSearch extends Component {
             token: ""
         };
     }
+
 
     //   askForNotificationPermission(){
     //     firebase.messaging().hasPermission()
@@ -51,18 +51,6 @@ export default class CalendarSearch extends Component {
     // }
 
     componentDidMount(){
-        axios.get(Server.url + 'api/lecturesDate')
-            .then(response => {
-                this.setState({
-                    showLectures: response.data,
-                });
-            }).catch(error => {
-            Toast.show({
-                text: 'Error reaching the server.',
-                type: "danger",
-                buttonText: 'Okay'
-            });
-        })
 
         // firebase.messaging().getToken()
         //     .then(fcmToken => {
@@ -91,6 +79,108 @@ export default class CalendarSearch extends Component {
         //
         // }
 
+    }
+
+    
+    
+    componentDidMount(){
+        
+        axios.get(Server.url + 'api/lecturesDate')
+            .then(response => {
+                this.setState({
+                    showLectures: response.data,
+                });
+            }).catch(error => {
+            Toast.show({
+                text: 'Error reaching the server.',
+                type: "danger",
+                buttonText: 'Okay'
+            });
+        })
+    
+        this.notificationPermission();
+
+        firebase.notifications().getInitialNotification()
+            .then((notificationOpen: NotificationOpen) => {
+                if (notificationOpen) {
+
+                }
+            });
+
+        // notification listner
+        this.pushNotiListner()
+
+        // notification open
+        this.onPushNotiOpen();
+
+        firebase.messaging().getToken()
+        .then(fcmToken => {
+            if(fcmToken){
+                this.state.token = fcmToken;
+                AsyncStorage.getItem('token').then(userToken => {
+                    axios.post(Server.url + 'api/updatetoken?token='+userToken,
+                        {token: this.state.token}
+                    )
+                        .then(response=>{
+    
+                        })
+                })
+            }else{
+                Toast.show({
+                    text: 'You must accept to send notification to you.',
+                    type: "danger",
+                    buttonText: 'Okay'
+                });
+            }
+        });
+    }
+
+    notificationPermission = () => {
+        firebase.messaging().hasPermission()
+            .then(enabled => {
+                if (enabled) {
+                    // user has permissions
+                    console.log('Permission already granted.')
+                } else {
+                    // user doesn't have permission
+                    console.log('No permission yet, Requesting...')
+                    firebase.messaging().requestPermission()
+                        .then(() => {
+                            // User has authorised
+                            console.log('Permission granted.')
+                        })
+                        .catch(error => {
+                            // User has rejected permissions
+                            console.log('permission denied')
+                            console.log(error)
+                        });
+                }
+            });
+    }
+    pushNotiListner = () => {
+        this.notificationListener = firebase.notifications().onNotification((notification: Notification) => {
+            // Process your notification as required
+            console.log('three')
+            console.log(notification)
+            // Display the notification
+            notification.android.setChannelId('test-channel');
+            firebase.notifications().displayNotification(notification)
+        });
+        const channel = new firebase.notifications.Android.Channel('test-channel', 'Test Channel', firebase.notifications.Android.Importance.Max)
+            .setDescription('My apps test channel');
+        console.log(channel)
+        // Create the channel
+        firebase.notifications().android.createChannel(channel);
+    }
+    onPushNotiOpen = () => {
+        this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen: NotificationOpen) => {
+            console.log('Notifications has been opend >>>>>>>>>>>>>>>>')
+            //   this.props.navigation.navigate('Notifications')
+            // Get the action triggered by the notification being opened
+            const action = notificationOpen.action;
+            // Get information about the notification that was opened
+            const notification: Notification = notificationOpen.notification;
+        });
     }
 
     componentWillUnmount() {
